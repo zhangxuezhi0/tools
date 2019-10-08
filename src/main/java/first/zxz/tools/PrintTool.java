f93982cf4e8e639e451c8aa7496bff6428053647
zxz <1548239937@qq.com>
2019-10-08 10:03:27 +0800

    Add print tool class file.

---
+++ src/main/java/first/zxz/tools/PrintTool.java
@@ -0,0 +1,26 @@
+package first.zxz.tools;
+
+/**
+ * /**
+ * 格式化输出工具
+ *
+ * @author zhangxz
+ * 2019/9/29
+ */
+public class PrintTool {
+
+    /**
+     * 输出int数组
+     *
+     * @param ints 数组
+     * @return void
+     * @author Zxz
+     * @date 2019/9/29 20:31
+     **/
+    public static void printIntArr(int[] ints) {
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+}
